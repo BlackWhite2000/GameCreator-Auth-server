8a5b15037b5dee1973c2b357e81c9d10fc3e7e1a
BlackWhite <2285697543@qq.com>
2025-01-10 08:16:03 +0800

    v2

--- app.py
+++ app.py
@@ -1,37 +1,52 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
-import json
-from getting.response import message_status
-from getting.gamecreator.auth import login_pw, login_token
-from getting.auth import token, verify_token
-from datetime import datetime
-import pymysql
-from env import db_config
-from pymysql import Error
-from getting.pymysql.sql import query, insert, update
+# 依赖库
+from flask import Flask, jsonify, request, Blueprint
+from loguru import logger
 
-app = Flask(__name__)
-# cors = CORS(app)
+# 创建数据库连接，封装了 from sqlalchemy import create_engine
+"""
+def create_session(url):
+    engine = create_engine(url, echo=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+"""
+from utils.database import create_session
 
+# 数据库地址
+from config.database import GAMECREATOR_MYSQL_DATABASE_URL
 
-@app.route("/apis/gamecreator/auth/login", methods=["POST"])
+# 表
+from models.gamecreator import GameCreatorAuth
+
+# auth_token key
+from config.auth import GAMECREATOR_AUTH_SECRET_KEY 
+
+ # 可在getting库中找到相同代码，此处是将库的函数放到了本地。目的是为了编译后的虚拟环境能更小点，因为那个库用到太多依赖了。
+from utils.response import message_status
+from utils.auth import token, verify_token
+from utils.gamecreator.auth import login_pw, login_token, login_code
+
+gamecreator_auth = Blueprint('gamecreator_auth', __name__)
+
+@gamecreator_auth.route("/apis/gamecreator/auth/login", methods=["POST"])
 def login():
-    "生成或返回 auth_token"
     """
+    生成或返回 auth_token
+
     auth_token 是一串全新随机的字符串，目的是用于验证使用相关服务时候的所有权。
     这样就不需要频繁的去请求GameCreator官方登陆系统来验证账号。
     """
-
-    # 获取JSON数据
     data = request.get_json()
     request_type = int(data.get("type", 0))
-    username = data.get("username", None)
+    username = data.get("username", None)  # 手机号、邮箱、用户名
     password = data.get("password", None)
     user_token = data.get("token", None)
+    phone = data.get("phone", None)
+    code = data.get("code", None)
 
-    if request_type != 0 | request_type != 1:
+    if request_type != 0 and request_type != 1 and request_type != 2:
         return jsonify(message_status(None, "请求类型错误"))
-
+    
     user_data = None
     if request_type == 1 and username is not None and password is not None:
         # 使用账号密码登录GC平台
@@ -40,23 +55,23 @@ def login():
     if request_type == 0 and user_token is not None:
         # 使用token登录GC平台
         user_data = login_token(user_token)
+    
+    if request_type == 2 and phone is not None and code is not None:
+        # 使用手机短信登录GC平台 - 需自行获取短信验证码
+        user_data = login_code(phone, code)
 
     if user_data is None:
-        # 如果数据空
         return jsonify(message_status(None, "登录信息错误-1"))
-
+    
     request_code = user_data.get("code", None)
     if request_code is None:
-        # 如果请求状态为空
         return jsonify(message_status(None, "登录信息错误-2"))
 
     if request_code != 20000:
-        # 如果请求失败
         return jsonify(message_status(None, "请求失败"))
 
     request_data = user_data.get("data", None)
     if request_data is None:
-        # 如果请求数据为空
         return jsonify(message_status(None, "请求数据为空"))
 
     uid = request_data.get("id")
@@ -66,165 +81,79 @@ def login():
         "username": user_name,
     }
 
-    # 连接到数据库
-    connection = pymysql.connect(**db_config())
-    # 创建游标
-    cursor = connection.cursor()
-
+    session = create_session(GAMECREATOR_MYSQL_DATABASE_URL)
     try:
-        # 密钥
-        query_sql = """
-        SELECT *
-        FROM auth_secret_key
-        WHERE type = 'gamecreator_auth';
-        """
-        execute, auth_key = query(cursor, query_sql)
-        if execute < 1:
-            return jsonify(message_status(None, "key不存在, 请联系开发者"))
-        auth_key = auth_key[0]
-
         # 尝试获取现有记录
-        query_sql = """
-        SELECT *
-        FROM gamecreator_auth
-        WHERE uid =  %(uid)s;
-        """
-        execute, auth_data = query(cursor, query_sql, ({"uid": uid}))
+        auth = session.query(GameCreatorAuth).filter(GameCreatorAuth.uid == uid).first()
 
-        if execute < 1:
+        if auth is None:
             # 如果记录不存在，创建一个新记录
-            insert_sql = """
-            INSERT INTO gamecreator_auth (uid, username, auth_token, updated_at)
-            VALUES (%(uid)s, %(username)s, %(auth_token)s, %(updated_at)s);
-            """
-            execute, auth_data = insert(
-                insert_sql,
-                (
-                    {
-                        "uid": uid,
-                        "username": user_name,
-                        "auth_token": token(auth_key.get("secret_key"), payload),
-                        "updated_at": datetime.now(),
-                    }
-                ),
-            )
-            # 提交更改
-            connection.commit()
-            query_sql = """
-            SELECT *
-            FROM gamecreator_auth
-            WHERE uid = %(uid)s;
-            """
-            execute, auth_data = query(cursor, query_sql, ({"uid": uid}))
-            auth_data = auth_data[0]
-
+            payload["auth_token"] = token(GAMECREATOR_AUTH_SECRET_KEY, payload)
+            auth = GameCreatorAuth(**payload)
+            session.add(auth)
         else:
-            auth_data = auth_data[0]
             # GC平台用户名是否变更
-            if user_name != auth_data.get("username"):
-                update_sql = """
-                UPDATE gamecreator_auth
-                SET username = %(username)s, updated_at = %(updated_at)s
-                WHERE uid =  %(uid)s;
-                """
-                update(
-                    cursor,
-                    update_sql,
-                    (
-                        {
-                            "username": user_name,
-                            "updated_at": datetime.now(),
-                        }
-                    ),
-                )
-                
-                # 提交更改
-                connection.commit()
-                query_sql = """
-                    SELECT *
-                    FROM gamecreator_auth
-                    WHERE uid = %(uid)s;
-                    """
-                execute, auth_data = query(cursor, query_sql, ({"uid": uid}))
-                auth_data = auth_data[0]
+            if auth.username != user_name:
+                auth.username = user_name
+            # auth_token 是否过期
+            if not verify_token(GAMECREATOR_AUTH_SECRET_KEY, auth.auth_token):
+                auth.auth_token = token(GAMECREATOR_AUTH_SECRET_KEY, payload)
+        
+        session.commit()
+        return jsonify(message_status({
+            "uid": auth.uid,
+            "username": auth.username,
+            "auth_token": auth.auth_token
+        }, "登录成功"))
 
-            # 验证 auth_token 是否过期失效
-            if not verify_token(
-                auth_key.get("secret_key"), auth_data.get("auth_token")
-            ):
-                update_sql = """
-                UPDATE gamecreator_auth
-                SET auth_token = %(auth_token)s, updated_at = %(updated_at)s
-                WHERE uid =  %(uid)s;
-                """
-                update(
-                    cursor,
-                    update_sql,
-                    (
-                        {
-                            "auth_token": token(auth_key.get("secret_key"), payload),
-                            "updated_at": datetime.now(),
-                            "uid": uid,
-                        }
-                    ),
-                )
-                
-                # 提交更改
-                connection.commit()
-                query_sql = """
-                    SELECT *
-                    FROM gamecreator_auth
-                    WHERE uid = %(uid)s;
-                    """
-                execute, auth_data = query(cursor, query_sql, ({"uid": uid}))
-                auth_data = auth_data[0]
-
-        return jsonify(message_status(auth_data, "登录成功"))
-
-    except Error as e:
-        print("Error:", e)
+    except Exception as e:
+        logger.error(f"数据库错误 => {e}")
+        session.rollback()  
+        return jsonify(message_status(None, "数据库错误"))
+    finally:
+        session.close()
 
 
-@app.route("/apis/gamecreator/auth/status", methods=["POST"])
+@gamecreator_auth.route("/apis/gamecreator/auth/status", methods=["POST"])
 def status():
-    "查询登录状态"
+    """
+    查询登录状态
+
+    通过查询前端发送的auth_token是否有效
+    """
     data = request.get_json()
     auth_token = data.get("auth_token")
 
-    if auth_token is None:
+    if not auth_token:
+        # 直接判断 auth_token 是否为空
         return jsonify(message_status(None, "提交信息为空"))
+    
+    if not verify_token(GAMECREATOR_AUTH_SECRET_KEY, auth_token):
+        # 如果 token 无效，直接返回
+        return jsonify(message_status(None, "auth_token无效"))
+    
+    session = create_session(GAMECREATOR_MYSQL_DATABASE_URL)
+    try:
+        # 尝试根据 auth_token 查询用户
+        auth = session.query(GameCreatorAuth).filter(GameCreatorAuth.auth_token == auth_token).first()
 
-    # 连接到数据库
-    connection = pymysql.connect(**db_config())
-    # 创建游标
-    cursor = connection.cursor()
+        if auth is None:
+            # 如果没有找到相应的记录
+            return jsonify(message_status(None, "账号不存在"))
+        
+        return jsonify(message_status({
+            "uid": auth.uid,
+            "username": auth.username,
+            "auth_token": auth.auth_token
+        }, "auth_token有效"))
 
-    # 密钥
-    query_sql = """
-    SELECT *
-    FROM auth_secret_key
-    WHERE type = 'gamecreator_auth';
-    """
-    execute, auth_key = query(cursor, query_sql)
-    if execute < 1:
-        return jsonify(message_status(None, "key不存在, 请联系开发者"))
-    auth_key = auth_key[0]
-
-    # 验证auth_token是否过期失效
-    if verify_token(auth_key.get("secret_key"), auth_token):
-        # 尝试获取现有记录
-        query_sql = """
-        SELECT *
-        FROM gamecreator_auth
-        WHERE auth_token =  %(auth_token)s;
-        """
-        execute, auth_data = query(cursor, query_sql, ({"auth_token": auth_token}))
-        if execute < 1:
-            return jsonify(message_status(None, "auth_token 不存在, 请尝试重新登录"))
-        return jsonify(message_status(auth_data[0], "登录成功"))
-    else:
-        return jsonify(message_status(None, "登录信息错误, 或 auth_token 过期"))
-
+    except Exception as e:
+        logger.error(f"数据库错误 => {e}")
+        return jsonify(message_status(None, "数据库错误"))
+    finally:
+        session.close()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9002)
+    app = Flask(__name__)
+    app.register_blueprint(gamecreator_auth)
+    app.run(port=9002, debug=True)
